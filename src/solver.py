1fc7a195058b19f0f4ade72f391e84dfd64fdede
Jinay Sarawagi <jinays1408@gmail.com>
2026-09-05 00:01:14 +0530

    solver.py

--- src/solver.py
+++ src/solver.py
@@ -1,17 +1,4 @@
 def count_neighbors(grid, row, col):
-    """
-    Counts the number of alive neighbors for a specific cell in the grid.
-    A cell can have up to 8 neighbors (horizontal, vertical, and diagonal).
-
-    Args:
-        grid (list of lists): The current 2D state of the game.
-        row (int): The row index of the cell.
-        col (int): The column index of the cell.
-
-    Returns:
-        int: The total number of alive neighbors (0 to 8).
-    """
-
     alive_count = 0
 
     rows = len(grid)
@@ -37,16 +24,7 @@ def count_neighbors(grid, row, col):
 
 #---------------------------- TASK 2 ----------------------------
 def compute_next_generation(grid):
-    """
-    Generates the next state of the grid based on Conway's rules.
-
-    Args:
-        grid (list of lists): The current 2D state of the game.
-
-    Returns:
-        list of lists: A BRAND NEW 2D grid representing the next generation.
-    """
-
+    
     rows = len(grid)
     cols = len(grid[0]) if rows > 0 else 0
 
